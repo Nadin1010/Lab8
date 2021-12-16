80d58120b10952629e262d03785b294d2769d108
Галина <@->
2021-12-16 06:26:00 +0300

    Добавьте файлы проекта.

---
+++ Lab8.2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab8._2
+{
+    class program
+    {
+        static void Main(string[] args)
+
+        {//програмно создаем текстовый файл. записываем в него 10 случайных чисел//
+            string path = "Task_8-2.txt";
+            Random random = new Random();
+            int S = 0;
+            using (StreamWriter sw = new StreamWriter(path))
+            { for (int i = 0; i < 10; i++)
+                {
+                    int x = random.Next(0, 10);
+                        sw.WriteLine(x);
+                }
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        int x = Convert.ToInt32(sr.ReadLine());
+                        S += x;
+                    }
+                    Console.WriteLine(S);
+                    Console.ReadKey();
+                }
+            }
+
+        }
+    }
+}
+
+
+
+
---
+++ Lab8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string dirName = "D:\\";
+
+            if (Directory.Exists(dirName))
+            {
+                Console.WriteLine("Подкаталоги:");
+                string[] dirs = Directory.GetDirectories(dirName);
+                foreach (string s in dirs)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            Console.ReadLine();
+            Console.ReadKey();
+         }
+    }
+}
+               
